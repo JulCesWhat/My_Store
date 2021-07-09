43ac90697d076c224bc104d27072d32765eb0b6e
Whatley, Julio (jw970q) <jw970q@att.com>
2021-07-09 17:58:58 -0400

    Adding file structure for application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ConfirmationComponent } from './pages/confirmation/confirmation.component';
+import { ProductItemComponent } from './pages/product-item/product-item.component';
+import { ProductItemDetailsComponent } from './pages/product-item-details/product-item-details.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    HeaderComponent,
+    ConfirmationComponent,
+    ProductItemComponent,
+    ProductItemDetailsComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
